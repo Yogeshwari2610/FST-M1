94879f86d85016167052d85ed4718e191476e77a
Yogeshwari2610 <B.P.Yogeshwari@ibm.com>
2024-06-13 13:14:52 +0530

    Add files via upload

---
+++ JUnit/Activities/Activity1.java
@@ -0,0 +1,5 @@
+package com.ecample.fst_m1_junit01;
+
+public class Activity1 {
+
+}
---
+++ JUnit/Activities/Calculator.java
@@ -0,0 +1,11 @@
+package com.ecample.fst_m1_junit01;
+
+public class Calculator {
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+    
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
